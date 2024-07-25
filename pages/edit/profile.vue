0ea22d227b09a8a3dcfb2aa707e4727373c2cca3
Tomas Barak <tbarak@b2gov.com>
2024-07-24 23:41:17 -0300

    Continue with profile edit page, next time finish sending the updated profile to the backend

--- pages/edit/profile.vue
+++ pages/edit/profile.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
     import NormalInput from '@/components/ui/edit/profile/NormalInput.vue'
+    import { isEqual, cloneDeep } from 'lodash'
 
     const user = toRaw(useAuthStore().user)
-    const profile: any = toRaw(useState('userProfile').value)
+    // const profile: any = toRaw(useState('userProfile').value)
 
-    // const profile: any = {
-    //     "user_id": "lxfAgybtGDNpSINDa7b59uWJKmz2",
-    //     "email": "mascotasenadopcionargentina@gmail.com",
-    //     "creation_instance": 10,
-    //     "name": "Tomás",
-    //     "surname": "Barak",
-    //     "accountType": 1,
-    //     "shelter_name": "Mascotas en Adopcion Argentina ",
-    //     "photo_url": "http://www.mascotasenadopcion.com/docs/logomascotasenadopcion180.jpg",
-    //     "short_description": "Mascotas en adopción se formó inicialmente con el fin de poder rescatar, rehabilitar y luego dar en adopción aquellos perros, gatos abandonados o maltratados en la zona de Del Viso, Partido de Pilar, aunque con el transcurrir del tiempo, hemos extendido nuestro trabajo abarcando Gran Buenos Aires y CABA.",
-    //     "website": "www.mascotasenadopcion.com/",
-    //     "social": {
-    //         "instagram": "mascotasenadopcionargentina",
-    //         "facebook": "mascotasenadopcionargentina",
-    //         "twitter": "mascotasenadopcionargentina"
-    //     },
-    //     "accepted_terms": true,
-    //     "is_verified_identity": true,
-    // }
+    const profile: any = {
+        "user_id": "lxfAgybtGDNpSINDa7b59uWJKmz2",
+        "email": "mascotasenadopcionargentina@gmail.com",
+        "creation_instance": 10,
+        "name": "Tomás",
+        "surname": "Barak",
+        "accountType": 2,
+        "shelter_name": "Mascotas en Adopcion Argentina",
+        "photo_url": "http://www.mascotasenadopcion.com/docs/logomascotasenadopcion180.jpg",
+        "short_description": "Mascotas en adopción se formó inicialmente con el fin de poder rescatar, rehabilitar y luego dar en adopción aquellos perros.",
+        "website": "www.mascotasenadopcion.com/",
+        "social": {
+            "instagram": "mascotasenadopcionargentina",
+            "facebook": "mascotasenadopcionargentina",
+            "twitter": "mascotasenadopcionargentina"
+        },
+        "accepted_terms": true,
+        "is_verified_identity": true,
+    }
+
+    const editProfile = reactive(cloneDeep(profile))
+
+    const hasDataChanged = ref(false)
 
     const stats: any = {
         "followers": 95000,
@@ -87,6 +92,45 @@
     const parseDisplayName = (name: string, surname: string) => {
         return `${parseName(name)} ${parseSurname(surname)}`
     }
+
+    function isObject(value: any): boolean {
+        return value && typeof value === 'object' && !Array.isArray(value);
+    }
+
+    function getChangedFields(original: any, edited: any): any {
+        const changes: any = {};
+
+        for (const key in edited) {
+            if (isObject(edited[key]) && isObject(original[key])) {
+                const nestedChanges = getChangedFields(original[key], edited[key]);
+                if (Object.keys(nestedChanges).length > 0) {
+                    changes[key] = nestedChanges;
+                }
+            } else if (!isEqual(original[key], edited[key])) {
+                changes[key] = edited[key];
+            }
+        }
+
+        return changes;
+    }
+
+    watch(editProfile, (newVal, oldVal) => {
+        // Compare with profile
+        hasDataChanged.value = !isEqual(toRaw(editProfile), profile)
+    }, {deep: true})
+
+    const handleProfileUpdate = async () => {
+        const changedFields = getChangedFields(profile, editProfile)
+        
+        if (Object.keys(changedFields).length > 0) {
+            console.log("CHANGED FIELDS")
+            console.log(changedFields)
+
+            //TODO: IMPLEMENTAR ENVIO DE DATOS A BACKEND
+        } else {
+            console.log("¿Que carajo hacés? Todavía no cambiaste nada... ¿Cómo llegaste hasta acá? Volá de acá")
+        }
+    }
 </script>
 
 <template>
@@ -106,27 +150,23 @@
             <div class="w-full flex flex-col md:grid md:grid-cols-2 md:grid-flow-row md:auto-cols-fr gap-[25px] md:gap-[50px] pt-[9px]">
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Nombre</h3>
-                    <NormalInput class="max-w-full w-full h-[50px]" :placeholder="parseName(profile.name)" />
+                    <NormalInput v-model="editProfile.name" class="max-w-full w-full h-[50px]" placeholder="Nombre" />
                 </div>
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Apellido</h3>
-                    <NormalInput class="max-w-full w-full h-[50px]" :placeholder="parseSurname(profile.surname)" />
+                    <NormalInput v-model="editProfile.surname" class="max-w-full w-full h-[50px]" placeholder="Apellido" />
                 </div>
             </div>
         </div>
 
-        <div v-if="profile.accountType != 0"class="w-full">
+        <div v-if="profile.accountType == 1" class="w-full">
             <h1 class="text-2xl font-extrabold text-[#333] w-full">Detalles del refugio</h1>
             <div class="divider mt-0"</div>
             <div class="w-full flex flex-col md:grid md:grid-cols-2 md:grid-flow-row md:auto-cols-fr gap-[25px] md:gap-[50px] pt-[9px]">
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Nombre del refugio</h3>
-                    <NormalInput class="max-w-full w-full h-[50px]" :placeholder="profile.shelter_name ? profile.shelter_name : parseDisplayName(profile.name, profile.surname)" />
+                    <NormalInput v-model="editProfile.shelter_name" class="max-w-full w-full h-[50px]" />
                 </div>
-                <!-- <div class="">
-                    <h3 class="text-l font-semibold text-[#333] w-full">Apellido</h3>
-                    <NormalInput class="max-w-full w-full h-[50px]" :placeholder="profile.surname" />
-                </div> -->
             </div>
         </div>
 
@@ -136,21 +176,32 @@
             <div class="w-full flex flex-col md:grid md:grid-cols-2 md:grid-flow-row md:auto-cols-fr gap-[25px] md:gap-[50px] pt-[9px]">
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Instagram</h3>
-                    <SocialInput class="max-w-full w-full h-[50px]" socialNetworkBaseURL="https://instagram.com" :placeholder="profile.social.instagram || 'ejemplo'" type="url"/>
+                    <SocialInput class="max-w-full w-full h-[50px]" socialNetworkBaseURL="https://instagram.com" v-model="editProfile.social.instagram" placeholder="ejemplo" type="url"/>
                 </div>
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Facebook</h3>
-                    <SocialInput class="max-w-full w-full h-[50px]" socialNetworkBaseURL="https://facebook.com" :placeholder="profile.social.facebook || 'ejemplo'" type="url"/>
+                    <SocialInput class="max-w-full w-full h-[50px]" socialNetworkBaseURL="https://facebook.com" v-model="editProfile.social.facebook" placeholder="ejemplo" type="url"/>
                 </div>
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Twitter</h3>
-                    <SocialInput class="max-w-full w-full h-[50px]" socialNetworkBaseURL="https://twiiter.com" :placeholder="profile.social.twitter || 'ejemplo'" type="url"/>
+                    <SocialInput class="max-w-full w-full h-[50px]" socialNetworkBaseURL="https://twiiter.com" v-model="editProfile.social.twitter" placeholder="ejemplo" type="url"/>
                 </div>
                 <div class="">
                     <h3 class="text-l font-semibold text-[#333] w-full">Sitio web</h3>
-                    <WebsiteInput class="max-w-full w-full h-[50px]" :placeholder="profile.website || 'www.ejemplo.com'" type="url"/>
+                    <WebsiteInput class="max-w-full w-full h-[50px]" v-model="editProfile.website" placeholder='www.ejemplo.com' type="url"/>
+                </div>
+            </div>
+        </div>
+
+        <div v-if="profile.accountType != 0" class="w-full">
+            <h1 class="text-2xl font-extrabold text-[#333] w-full">Descripción corta</h1>
+            <div class="divider mt-0"</div>
+            <div class="w-full flex flex-col md:grid md:grid-cols-2 md:grid-flow-row md:auto-cols-fr gap-[25px] md:gap-[50px] pt-[9px]">
+                <div class="">
+                    <LongInput v-model="editProfile.short_description" maxlength="140" placeholder="Somos El Gran Refugio, nuestro propósito es..." type="text"/>
                 </div>
             </div>
         </div>
     </section>
+    <button :disabled="!toRaw(hasDataChanged)" @click="handleProfileUpdate" :class="{'bg-primary': toRaw(hasDataChanged), 'bg-[#d3d3d3]': !toRaw(hasDataChanged)}" class="fixed bottom-[15px] right-[15px] h-[50px] w-[100px] text-white p-0 m-0 rounded-[4px] transition-colors">Guardar</button>
 </template>
